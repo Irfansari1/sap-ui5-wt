3af062bc9278ecb7a29585948f99784b04bfcb83
Irfansari1 <irfansari42@gmail.com>
2022-07-11 23:08:28 +0200

    controls are added

--- webapp/index.js
+++ webapp/index.js
@@ -1,5 +1,11 @@
-sap.ui.define([], function () {
+sap.ui.define([
+
+  "sap/m/Text"
+
+], function (Text) {
   "use strict";
 
-  alert("UI5 is ready to go!");
+  new Text({
+    text:"Hello UI5!"
+  }).placeAt("content")
 });
