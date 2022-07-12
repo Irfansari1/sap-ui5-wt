d4f29edee6fe6a022703b157deb918666278b99c
Irfansari1 <irfansari42@gmail.com>
2022-07-12 11:25:17 +0200

    XML views added

--- webapp/index.js
+++ webapp/index.js
@@ -1,11 +1,9 @@
-sap.ui.define([
-
-  "sap/m/Text"
-
-], function (Text) {
+sap.ui.define(["sap/ui/core/mvc/XMLView"], function (XMLView) {
   "use strict";
 
-  new Text({
-    text:"Hello UI5!"
-  }).placeAt("content")
+  XMLView.create({
+    viewName: "sap.ui.demo.walkthrough.view.App"
+  }).then(function (oView) {
+    oView.placeAt("content");
+  });
 });
