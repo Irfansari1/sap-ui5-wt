f946038622ee8803910d3e54b53afdb74a39654b
Irfansari1 <irfansari42@gmail.com>
2022-07-17 01:25:14 +0200

    Descriptors2

--- webapp/component.js
+++ webapp/component.js
@@ -4,25 +4,22 @@ sap.ui.define([
   "sap/ui/model/resource/ResourceModel",
 ], function(UIComponent, JSONModel, ResourceModel){
     "use strict";
-    //all the rootview info under metadata is repleced by manifest, and delete index.js file, and delete i18n model in component
+    
     return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
-        metadata : {
-            rootView : {
-                manifest:"json"
-            }
-        },
-        init: function () {
-            //call the init function of the parent
-            UIComponent.prototype.init.apply(this, arguments);
-            //set data models
-            var oData = {
-                recipient: {
-                name: "UI5",
-                },
-            };
-            var oModel = new JSONModel(oData);
-            this.setModel(oModel);
-
-        }
-    })
+      metadata: {
+          manifest: "json"
+      },
+      init: function () {
+        //call the init function of the parent
+        UIComponent.prototype.init.apply(this, arguments);
+        //set data models
+        var oData = {
+          recipient: {
+            name: "UI5",
+          },
+        };
+        var oModel = new JSONModel(oData);
+        this.setModel(oModel);
+      },
+    });
 });
