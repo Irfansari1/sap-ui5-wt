f459fc32fa7c87aaa9cdda29b602e00effdbaacc
Irfansari1 <irfansari42@gmail.com>
2022-07-23 23:15:13 +0200

    Dialogs and fragments

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,9 +1,6 @@
 sap.ui.define(
-  [
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-  ],
-  function (Controller, MessageToast) {
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/core/Fragment"],
+  function (Controller, MessageToast, Fragment) {
     "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
@@ -19,6 +16,17 @@ sap.ui.define(
         //show a native or vanilla JS alert
         //alert("Hello there!");
         //MessageToast.show("Hello there!");
+      },
+      onOpenDialog: function () {
+        // create dialog lazily
+        if (!this.pDialog) {
+          this.pDialog = this.loadFragment({
+            name: "sap.ui.demo.walkthrough.view.HelloDialog",
+          });
+        }
+        this.pDialog.then(function (oDialog) {
+          oDialog.open();
+        });
       },
     });
   }
